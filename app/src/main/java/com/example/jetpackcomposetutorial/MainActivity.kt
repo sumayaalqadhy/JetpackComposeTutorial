c3f3c72e988bea86171647eece15b2a676b63604
sumayaalqadhy <sumaya.alqadhy@gmail.com>
2024-09-06 18:39:19 -0700

    added image

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,10 +16,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.Paragraph
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,14 +53,18 @@ fun TutorialText(heading: String, firstParagraph: String, secondParagraph: Strin
         modifier = modifier) {
         Text(
             text = heading,
+            fontSize = 24.sp,
             modifier = modifier
         )
         Text(
             text = firstParagraph,
+            fontSize = 14.sp,
             modifier = modifier
+                .padding(16.dp)
         )
         Text(
             text = secondParagraph,
+            fontSize = 14.sp,
             modifier = modifier
         )
     }
@@ -64,14 +72,27 @@ fun TutorialText(heading: String, firstParagraph: String, secondParagraph: Strin
 
 @Composable
 fun TutorialImage(heading: String, firstParagraph: String, secondParagraph: String, modifier: Modifier = Modifier){
-    TutorialText(
-        heading = heading,
-        firstParagraph = firstParagraph,
-        secondParagraph = secondParagraph,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp)
-    )
+    val image = painterResource(R.drawable.bg_compose_background)
+
+    Box(modifier = modifier
+        .fillMaxSize()
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+
+
+
+            )
+        TutorialText(
+            heading = heading,
+            firstParagraph = firstParagraph,
+            secondParagraph = secondParagraph,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
